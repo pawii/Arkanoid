3f81410736595184f91092c973faae14de8febcc
PAVEL\pawii <pawii@yandex.ru>
2022-10-16 20:44:41 +0300

    create ThirdPerson template

---
+++ Arkanoid/Source/Arkanoid/Arkanoid.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Arkanoid.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Arkanoid, "Arkanoid" );
+ 
---
+++ Arkanoid/Source/Arkanoid/ArkanoidGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArkanoidGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AArkanoidGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AArkanoidGameMode();
+};
+
+
+
