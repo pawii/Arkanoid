b5a5ae5736ce951389bba3d8f679c862636dc8cf
PAVEL\pawii <pawii@yandex.ru>
2022-10-19 00:20:31 +0300

    create main custom GameFramework classes and blueprints for them

---
+++ Arkanoid/Source/Arkanoid/Private/ArkanoidPlayerCameraManager.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/PlayerCameraManager.h"
+#include "ArkanoidPlayerCameraManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AArkanoidPlayerCameraManager : public APlayerCameraManager
+{
+	GENERATED_BODY()
+};
